009d7fcfc64de5c894bf8439ccc30ef7dbbaa17a
IGN-Styly <claudioresendeptbr@gmail.com>
2023-05-23 20:27:45 +0100

    Some work

--- src/components/bars.tsx
+++ src/components/bars.tsx
@@ -8,10 +8,11 @@ const NavBar: NextPage = () => {
     <><div className="top-0">
     <header className="p-4 border-b border-overlay flex justify-between text-sm font-semibold top-0 mb-5">
         <div className="space-x-6 flex items-center">
-            <Link className="text-xl leading-none text-[#cdd6f4]" href="/">
+            <Link className="text-xl leading-none text-text hover:text-lavender" href="/">
                 My Crypto
             </Link>
-            {/* <Link className=" leading-none text-textcc" href={"/api/auth/signin"}>Puzza Klient</Link> */}
+            <Link className="leading-none text-subtext1 hover:text-lavender" href={"/wiki"}>Wiki</Link>
+            <Link className="leading-none text-subtext1 hover:text-lavender" href={"https://ign-styly.xyz"}>Blog</Link>
            
         </div>
         <span className="text-gray-400">{sessionData?.user?.name ? "Welcome, "+sessionData?.user?.name+"!":<></>}</span>
